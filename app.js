49cbf324c9349ba90b2332821873c23fed69d75b
Ryan Mwakio <ryanmwakio6@gmail.com>
2022-05-20 21:20:31 +0300

    configure cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
+var cors = require("cors");
 
 var indexRoutes = require("./routes/index");
 var authRoutes = require("./routes/auth");
@@ -16,20 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //setting up to allow all cors and origin
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Expose-Headers", "x-auth-token");
-  res.header("preflight", "true");
-
-  next();
-});
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
